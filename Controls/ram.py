cd538fefde6922f873044946f1bfd3e33b5bef4b
Nicolò <brunonico787@gmail.com>
2024-11-18 10:06:26 +0100

    Update Controls

--- Controls/ram.py
+++ Controls/ram.py
@@ -10,25 +10,35 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import thresholds
 
 
-ram = psutil.virtual_memory()
-
 #memoria disponibile ossia la memoria che può essere data istantaneamente ai processi senza che
 #il sistema entri in swap
-available = ram.available
+def available():
+    ram = psutil.virtual_memory()
+    return ram.available
+
 #percentuale mem = (tot-available)/tot *100
-percentage = ram.percent
+def percentage():
+    ram = psutil.virtual_memory()
+    return ram.percent
+
 #memoria attiva è la memoria attualmente in uso e quindi è in RAM
-active = ram.active
+def active():
+    ram = psutil.virtual_memory()
+    return ram.active
+
 #memoria inattiva è la memoria non utilizzata
-inactive = ram.inactive
+def inactive():
+    ram = psutil.virtual_memory()
+    return ram.inactive
 
 
-def get_ram_priority_level():
+
+def get_priority():
     ram = psutil.virtual_memory()
 
     available = ram.available
 
     if available >= thresholds.RAM:
-        return(1)  #livello di priorità 1 ossia alto
+        return "⚠️"
     else:
-        return(3)  #livello di priorità 3 ossia bassa
+        return ""
