38efa09e59fe4064f92dde67482eef7636f16911
Maria Pia <mariapiasaliani@gmail.com>
2024-11-18 09:13:39 +0100

    Add general_info.py and update thresholds.py

---
+++ Controls/general_info.py
@@ -0,0 +1,26 @@
+import psutil
+import time
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import thresholds
+
+
+def temperature():
+    temp = psutil.sensors_temperatures(fahrenheit=False)["acpitz"][0].current
+    return temp
+
+
+def get_priority():
+    if temperature() >= thresholds.TEMPERATURE:
+        return "⚠️"
+    else:
+        return ""
+
+
+def get_uptime():
+    uptime = time.time() - psutil.boot_time() # Uptime in secondi
+    uptime_hours = uptime // 3600  # Ore intere
+    uptime_minutes = (uptime % 3600) // 60  # Minuti rimanenti
+    return f"{uptime_hours}h {uptime_minutes}m"
--- thresholds.py
+++ thresholds.py
@@ -1,9 +1,10 @@
 
+# Thresholds
 CPU = 85 # cpu usage percentage
-
 RAM = 10 # available ram percentage
-
 DISKS = 10 # disk free space in GB
+TEMPERATURE = 45 # system temperature in Celsius
+
 
 
 
